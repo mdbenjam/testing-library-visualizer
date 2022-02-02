aa49fca0d268b5fcc5f91483be2d7bbefe48e58b
Mark Benjamin <mark@pop-os.localdomain>
2022-02-02 17:05:06 -0500

    Removing unneeded import

--- test-runner/src/App.test.js
+++ test-runner/src/App.test.js
@@ -3,7 +3,6 @@ import App from "./App";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import userEvent from "@testing-library/user-event";
-import { debugTest } from "react-testing-visualizer";
 
 let mockAxios = null;
 

cc6bcc2bc5b2f29340574943b670f6fd97ee3ec7
Mark Benjamin <mark@pop-os.localdomain>
2022-02-02 17:03:30 -0500

    Adding documentation

--- test-runner/src/App.test.js
+++ test-runner/src/App.test.js
@@ -3,6 +3,7 @@ import App from "./App";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import userEvent from "@testing-library/user-event";
+import { debugTest } from "react-testing-visualizer";
 
 let mockAxios = null;
 

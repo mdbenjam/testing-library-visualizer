56e1cef359882d4cde5696fc4bd81964eaf03b93
Mark Benjamin <mdbenjam@gmail.com>
2022-02-04 14:51:07 -0500

    test: test new editor command history fix

--- test-runner/src/Editor.test.js
+++ test-runner/src/Editor.test.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { render, screen } from "@testing-library/react";
 import Editor from "./Editor";
 import userEvent from "@testing-library/user-event";
-
+import { debugTest } from "react-testing-visualizer";
 const EditorWrapper = (props) => {
   const [content, setContent] = useState("");
 
@@ -33,4 +33,27 @@ test("can render error tooltip in editor", async () => {
   );
   await user.hover(await screen.findByText(/hello/));
   expect(await screen.findByText(/This is an error/)).toBeInTheDocument();
+});
+
+test("command history is reset when changed", async () => {
+  const user = userEvent.setup();
+
+  const { rerender } = render(<EditorWrapper commandHistory={["old"]} />);
+
+  await user.click(screen.getByRole("textbox"));
+  await user.keyboard("[ControlLeft>][ArrowUp][/ControlLeft]");
+
+  await expect(await screen.findByText(/old/)).toBeInTheDocument();
+
+  rerender(
+    <EditorWrapper
+      errors={[{ message: "This is an error", line: 1 }]}
+      commandHistory={["new", "old"]}
+    />
+  );
+
+  await user.click(screen.getByRole("textbox"));
+  await user.keyboard("[ControlLeft>][ArrowUp][/ControlLeft]");
+
+  await expect(await screen.findByText(/new/)).toBeInTheDocument();
 });
